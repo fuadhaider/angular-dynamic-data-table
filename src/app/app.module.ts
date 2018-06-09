f3e001abd412f4d02f141577a9a0579dd478601a
fuadhaider <mohaiderfuad@gmail.com>
2018-06-09 19:20:18 +0100

    filter done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TableGroupComponent } from './table-group/table-group.component';
@@ -12,6 +13,7 @@ import { LoadDataService } from './table-group/load-data.service';
     TableGroupComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     HttpClientModule
   ],
--- src/app/table-group/table-group.component.ts
+++ src/app/table-group/table-group.component.ts
@@ -10,7 +10,9 @@ import { LoadDataService } from './load-data.service';
 })
 export class TableGroupComponent implements OnInit {
   allStatus: any = [];
-
+  ss:any = [];
+  query = "";
+  filteredList;
   pageLength: number = 5;
   sliceStart: number;
   sliceEnd: number;
@@ -27,6 +29,32 @@ export class TableGroupComponent implements OnInit {
       console.log(this.allStatus);
     });
   }
+
+  filterData() {
+    this.loadDataService.getUrlData().subscribe(data => {
+      this.ss = data;
+
+      this.allStatus = this.ss.filter(function(el){
+        var result="";
+          for(var key in el){
+            result+= el[key];
+          }
+          return result.toLowerCase().indexOf(this.query.toLowerCase()) > -1;
+      }.bind(this));
+    });
+  }
+
+  // filterData(value:string) {
+  //   this.loadDataService.getUrlData().subscribe(data => {
+  //     this.allStatus = data;
+  //     for (var i = 0; i < this.allStatus.length; i++) {
+  //         if (this.allStatus[i].title === value) {
+  //             return this.allStatus[i];
+  //         }
+  //     }
+  //     return null;
+  //   });
+  // }
 
   sortNumber() {
     this.loadDataService.getUrlData().subscribe(data => {
